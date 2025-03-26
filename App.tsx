f3a7a55fea3409cd232bb1fa4f791f433d2b79e7
Shamsiddin27 <aleksandr.asad7@gmail.com>
2025-03-26 15:37:00 +0500

    Click transition to notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  createNavigationContainerRef,
+} from '@react-navigation/native';
 import SingUpNavigation from './src/navigation/sign-up.navigation';
 import useAuth from './src/hooks/useAuth';
 import AuthContext from './src/contexts/AuthContext';
@@ -15,6 +18,9 @@ import {ModalProvider} from './src/ui-components/modal/modal.provider';
 import messaging from '@react-native-firebase/messaging';
 import * as Sentry from '@sentry/react-native';
 import * as amplitude from '@amplitude/analytics-react-native';
+import notifee, {EventType, AndroidImportance} from '@notifee/react-native';
+
+export const navigationRef = createNavigationContainerRef();
 let ExportedApp;
 
 if (!__DEV__) {
@@ -70,6 +76,79 @@ function App() {
     cart,
     setCart,
   } = useAuth();
+  useEffect(() => {
+    const onNotificationPress = async (notification: any) => {
+      console.log('Нажатие на уведомление:', notification);
+
+      if (notification?.data?.orderId) {
+        const orderId = notification.data.orderId;
+        console.log(
+          `Клик по уведомлению. Переход на DetailOrder с ID: ${orderId}`,
+        );
+
+        if (navigationRef.isReady()) {
+          navigationRef.navigate('DetailOrder', {orderId});
+        }
+      } else {
+        console.log('orderId не найден в уведомлении');
+      }
+    };
+
+    const unsubscribeOnMessage = messaging().onMessage(async remoteMessage => {
+      console.log('Получено сообщение:', remoteMessage);
+
+      await notifee.requestPermission();
+      const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+        importance: AndroidImportance.HIGH,
+      });
+
+      await notifee.displayNotification({
+        title: remoteMessage.notification?.title || 'Новое уведомление',
+        body: remoteMessage.notification?.body || 'У вас новое сообщение',
+        android: {
+          channelId,
+          importance: AndroidImportance.HIGH,
+          pressAction: {
+            id: 'order', // Уникальный ID для обработки нажатий
+          },
+        },
+        data: remoteMessage.data, // Передаем orderId в data
+      });
+
+      if (remoteMessage.data?.orderId) {
+        console.log(
+          `Поступило уведомление с orderId: ${remoteMessage.data.orderId}`,
+        );
+      }
+    });
+
+    // Обработка кликов по уведомлениям в активном состоянии
+    const unsubscribeOnNotificationPress = notifee.onForegroundEvent(
+      ({type, detail}) => {
+        if (type === EventType.PRESS) {
+          onNotificationPress(detail.notification);
+        }
+      },
+    );
+
+    // Обработка кликов по уведомлениям в фоновом режиме
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Фоновое сообщение:', remoteMessage);
+    });
+
+    notifee.onBackgroundEvent(async ({type, detail}) => {
+      if (type === EventType.PRESS) {
+        onNotificationPress(detail.notification);
+      }
+    });
+
+    return () => {
+      unsubscribeOnMessage();
+      unsubscribeOnNotificationPress();
+    };
+  }, []);
   return (
     <AuthContext.Provider
       value={{
@@ -95,7 +174,7 @@ function App() {
               {!isLoading ? (
                 <ActivityIndicator size={'large'} color={colors.black} />
               ) : (
-                <NavigationContainer>
+                <NavigationContainer ref={navigationRef}>
                   {token ? <MainNavigation /> : <SingUpNavigation />}
                 </NavigationContainer>
               )}
